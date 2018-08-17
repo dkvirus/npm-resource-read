3d5f0a977f24c37aa5eb3241db62b324b1e60362
dkvirus <949582269@qq.com>
2018-08-17 22:55:04 +0800

    read: bin/npm-cli.js

--- bin/npm-cli.js
+++ bin/npm-cli.js
@@ -1,7 +1,11 @@
 #!/usr/bin/env node
-;(function () { // wrapper in case we're in module_context mode
-  // windows: running "npm blah" in this folder will invoke WSH, not node.
-  /* global WScript */
+
+/**
+ * 自执行函数
+ * 闭包内的变量作用域只在函数内，不会产生污染
+ */
+;(function () { 
+  // WScript 是 window 原生脚本对象，typeof WScript !== 'undefined' 判断当前系统是否是 window 系统
   if (typeof WScript !== 'undefined') {
     WScript.echo(
       'npm does not work when run\n' +
@@ -14,16 +18,26 @@
     return
   }
 
+  /** 
+   * process.title 属性用于获取或设置当前进程在 ps 命令中显示的进程名字
+   * npm install 安装包需要花费一段时间，此时 npm 会占用一个进程，使用 ps aux | grep npm 可以查看到进程
+   * dk: 查看到进程干嘛呢？？npm install 卡死关闭进程？？直接在终端 ctrl + c 不就中断运行了吗？why..
+  */
   process.title = 'npm'
 
   var unsupported = require('../lib/utils/unsupported.js')
-  unsupported.checkForBrokenNode()
+  // npm@6.2.0 不兼容 node@4.7.0 以下的版本，检测当前 node 版本如果低于 4.7.0，进程中止，报错，下面的代码不会被执行
+  unsupported.checkForBrokenNode()    
 
   var log = require('npmlog')
-  log.pause() // will be unpaused when config is loaded.
+  /**
+   * will be unpaused when config is loaded. dk: why do this？
+   * log.pause() 下面所有 log.info()  压根就不会在控制台打印出来，为啥还要写条日志信息？？
+   */
+  log.pause() 
   log.info('it worked if it ends with', 'ok')
 
-  unsupported.checkForUnsupportedNode()
+  unsupported.checkForUnsupportedNode()   // 检测当前 node 版本是否与 npm@6.2.0 兼容
 
   var path = require('path')
   var npm = require('../lib/npm.js')
@@ -37,12 +51,33 @@
 
   // if npm is called as "npmg" or "npm_g", then
   // run in global mode.
+  /**
+   * processs.argv 获取命令行参数，返回值是个数组
+   * 示例：npm install fs -g
+   *   [ 
+   *     '/Users/dkvirus/.nvm/versions/node/v8.11.3/bin/node',
+   *     '/Users/dkvirus/.nvm/versions/node/v8.11.3/bin/npm',
+   *     'install',
+   *     'fs',
+   *     '-g' 
+   *   ]
+   * 解析：
+   *    数组第一个值为 node 程序安装路径
+   *    数组第二个值为命令 npm 安装路径
+   *    数组第三个值以及之后的值为命令行参数
+   * 
+   * dk: 这里这个判断【path.basename(process.argv[1]).slice(-1) === 'g'】 有何意义？
+   * 按照官方的意思是本来想敲 `npm -g`，结果手误敲成 `npmg` 或者 `npm_g`，Are you kidding me..
+   * npmg 或者 npm_g 压根就没这个命令，控制台会直接报错的好吗？？这三行代码完全没有意义！！
+   */
   if (path.basename(process.argv[1]).slice(-1) === 'g') {
     process.argv.splice(1, 1, 'npm', '-g')
   }
 
+  // log.verbose() 也不打印，这里写这句话不觉明历，verbose 中文意思：冗长的，啰嗦的
   log.verbose('cli', process.argv)
 
+  // 解析命令行选项  https://npm.taobao.org/package/nopt
   var conf = nopt(types, shorthands)
   npm.argv = conf.argv.remain
   if (npm.deref(npm.argv[0])) npm.command = npm.argv.shift()
